cd436d9511153b73cba32e9fe8bc17780ef9e5d5
Roberto Pozzi <r.robipozzi@gmail.com>
2024-01-04 11:05:07 +0100

    remove Keycloak authentication tests

--- test/requestKeycloakToken.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST http://localhost:8081/realms/windfire-restaurants/protocol/openid-connect/token \
-  -H 'Accept: application/json' \
-  -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'cache-control: no-cache' \
-  -d 'client_id=<INSERT_REALM_ID>&username=<INSERT_REALM_USERNAME>&password=<INSERT_REALM_PASSWORD>&grant_type=password'
